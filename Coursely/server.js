dc450e299d205bcbb67498687f44085a3e14a344
Farouk <faroukkisuule55@gmail.com>
2021-09-08 20:34:29 -0400

    Updated package.json file with heroku env on deploy

--- Coursely/server.js
+++ Coursely/server.js
@@ -16,7 +16,10 @@ app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
    app.use(express.static("client/build"));
- }
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 app.use("/api/school", require("./routes/school"));
 
